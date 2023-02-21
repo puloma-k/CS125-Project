748342ea7f48ce1add79bfad97a68b97a4f650b5
Puloma Katiyar <puloma@pulomakatiyar.com>
2023-02-21 14:05:39 -0800

    Initial Commit

---
+++ MeMind/MeMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeMindApp.swift
+//  MeMind
+//
+//  Created by Puloma Katiyar on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MeMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
